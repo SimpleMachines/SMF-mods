3fbc9360419fcde853bf72fb9b0cba25a46ebee0
Bugo <bugo@dragomano.ru>
2021-08-23 13:25:11 +0500

    Add Russian translation
    Signed-off-by: Bugo <bugo@dragomano.ru>

---
+++ curve2-color-changer/ColorChanger.russian.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package Curve2 Color Changer
+ * @author SMF Customization Team (www.simplemachines.org)
+ * @license BSD 3-clause
+ * @version 1.0
+ */
+
+$txt['cc_color_changer'] = 'Палитра темы';
+
+$txt['cc_primary_color'] = 'Основная цветовая схема';
+$txt['cc_secondary_color'] = 'Дополнительная цветовая схема';
+$txt['cc_background'] = 'Цвет фона';
+$txt['cc_foreground'] = 'Цвет переднего плана';
+$txt['cc_top_section'] = 'Цвет фона шапки';
+$txt['cc_footer'] = 'Цвет фона подвала';
+$txt['cc_links'] = 'Цвет ссылок';
+$txt['cc_gradient_start'] = 'Начальный цвет градиента';
+$txt['cc_gradient_end'] = 'Конечный цвет градиента';
+$txt['cc_blocks_color'] = 'Цвет блоков';
+$txt['cc_blocks_alternate_color'] = 'Альтернативный цвет блоков';
+$txt['cc_borders_color'] = 'Цвет рамок';
+$txt['cc_special_titles_color'] = 'Цвет специальных заголовков';
+$txt['cc_buttons_text_color'] = 'Цвет текста кнопок';
+$txt['cc_buttons_bg'] = 'Цвет фона кнопок';
+$txt['cc_buttons_border'] = 'Цвет рамки кнопок';
+
+$txt['cc_remove_shadows'] = 'Удалить тени';
+$txt['cc_admin_only'] = 'Тестовый режим';
+$txt['cc_admin_only_help'] = 'Только админы смогут видеть изменения';
+
+$txt['cc_color_picker'] = 'Выбор цвета';
+$txt['cc_default_color'] = 'Использовать цвет по умолчанию';
+$txt['cc_reset_all'] = 'Сбросить к значениям по умолчанию';
+
+$txt['cc_palettes_title'] = 'Предустановленные цветовые палитры';
+$txt['cca_unknown_color'] = 'Неизвестный цвет';
