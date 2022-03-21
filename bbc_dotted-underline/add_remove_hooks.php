2e229a79ce7c507b01897e15b6e5514801f474e5
Diego Andrés <diegoandres_cortes@outlook.com>
2022-03-21 00:14:29 -0600

    add global
    signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- bbc_dotted-underline/add_remove_hooks.php
+++ bbc_dotted-underline/add_remove_hooks.php
@@ -15,6 +15,9 @@ if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
 	require_once(dirname(__FILE__) . '/SSI.php');
 elseif (!defined('SMF')) // If we are outside SMF and can't find SSI.php, then throw an error
 	die('<b>Error:</b> Cannot install - please verify you put this file in the same place as SMF\'s SSI.php.');
+
+	global $context;
+
 if (SMF == 'SSI')
 	db_extend('packages');
 
